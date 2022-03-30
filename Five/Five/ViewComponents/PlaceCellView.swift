39226ad10a4ddedea76ee06aebb5d2f7ab18b14f
Goga Eusebiu <eusebiu.goga@p3-group.com>
2022-03-30 17:30:48 +0300

    bold text in cell and text color change for better visibility

--- Five/Five/ViewComponents/PlaceCellView.swift
+++ Five/Five/ViewComponents/PlaceCellView.swift
@@ -18,23 +18,38 @@ struct PlaceCellView: View {
             ZStack() {
                 VStack() {
                     HStack {
-                        Text("distance \(place.distance) meters").font(.system(size: 12)).padding(.all)
+                        Text("distance \(place.distance) meters")
+                            .font(.system(size: 12).bold()).padding(.all)
+                            .foregroundColor(Color.white)
+
                         Spacer()
-                        Text(place.streetAddress ?? "").font(.system(size: 12).bold()).padding(.all)
+                        Text(place.streetAddress ?? "")
+                            .font(.system(size: 12).bold()).padding(.all)
+                            .foregroundColor(Color.white)
                     }
                     
-                    Text(place.name ?? "").font(.system(size: 16)).padding(.all)
-                    Text(place.locality ?? "").font(.system(size: 16)).padding(.all)
+                    Text(place.name ?? "")
+                        .font(.system(size: 18).bold()).padding(.all)
+                        .foregroundColor(Color.white)
+
+                    Text(place.locality ?? "")
+                        .font(.system(size: 18).bold()).padding(.all)
+                        .foregroundColor(Color.white)
                     
                     HStack {
-                        Text(place.region ?? "").font(.system(size: 12).bold()).padding(.all)
+                        Text(place.region ?? "")
+                            .font(.system(size: 12).bold()).padding(.all)
+                            .foregroundColor(Color.white)
+
                         Spacer()
-                        Text(place.country ?? "").font(.system(size: 12).bold()).padding(.all)
+                        Text(place.country ?? "")
+                            .font(.system(size: 12).bold()).padding(.all)
+                            .foregroundColor(Color.white)
                     }
                 }
-                .background(Color(.clear))
+                .background(Color.clear)
             }
-            .background(Color.white.opacity(0.4))
+            .background(Color.black.opacity(0.4))
             .frame(height: 200)
         }
     }
